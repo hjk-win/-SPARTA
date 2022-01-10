211504ae084640c38ba884cd105cf6b75a8e48f9
hjk-win <1287575516@qq.com>
2022-01-10 20:39:32 +0800

    add presenter role

---
+++ src/main/java/com/jkhan/sparta_easy_upload/bean/PresenterInfo.java
@@ -0,0 +1,19 @@
+package com.jkhan.sparta_easy_upload.bean;
+
+import lombok.Data;
+
+/**
+ * @ClassName PresenterInfo
+ * @Description PresenterInfo
+ * @Author hjk
+ * @Date 2022/1/8 17:09
+ **/
+@Data
+public class PresenterInfo {
+    private long presenterId;
+    private String presenterName;
+    private String presenterTelNumber;
+    private String presenterEmail;
+    private String presenterWechat;
+    private long collectorId;
+}
